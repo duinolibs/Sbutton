78446ecd536d90a7ee59f36626c8bb3af507f0d8
Stepko/ <sguralmen@gmail.com>
2022-04-23 16:11:13 +0300

    0.05

--- src/SButton.cpp
+++ src/SButton.cpp
@@ -1,57 +1,60 @@
 #include "SButton.h"
 void Sbutton::tick() {
-  hasClicks = false;
-  isHolded = false;  
-  isHold = false;
-  isHoldAfterClicks = 0;
-  isClicked = false;
+  _clicked = 0;
+  _held = 0;
+  _hold = 0;
+  _clicksEnd = 0;
+  _clicksWithHeld = 0;
+  _clicksWithHold = 0;
   uint32_t t = millis();
-  bool reading = digitalRead(pin) == (pinmode)? HIGH : LOW;
-   if (reading != lastState) {
-    lastChange = t;
+  bool reading = digitalRead(_pin) == (_pinmode) ? HIGH : LOW;
+  if (reading != _lastState) {
+    _lastChange = t;
   }
-  if(t-lastChange>50){
-        if (reading != isPressed) {
-      isPressed = reading;}
+  if (t - _lastChange > 50) {
+    if (reading != _pressed) {
+      _pressed = reading;
+    }
 
-  if (isPressed) {
-    switch (mode) {
-      case await:
-        mode = pressed;
-        timer = t;
-        clicks = 1;
-        isClicked = true;
-        break;
-      case pressed:
-        if (t - timer >= 500) {
-          mode = held;
-          timer = t;
-          isHolded = true;
-          isHoldAfterClicks = clicks-1;
-        }
-        break;
-      case held:
-        isHold = true;
-        break;
-      case released:
-        mode = pressed;
-        timer = t;
-        clicks ++;
-        isClicked = true;
-        break;
-    }
-  } else {
-    switch (mode) {
-      case pressed: mode = released; timer = t; isHold = false; break;
-      case held: mode = await; timer = t; isHold = false; break;
-      case released: if (t - timer >= 500) {
-          mode = await;
-          timer = t;
-          hasClicks = clicks;
-          clicks = 0;
-        } break;
+    if (_pressed) {
+      switch (mode) {
+        case await:
+          mode = pressed;
+          _timer = t;
+          _clicks = 1;
+          _clicked = 1;
+          break;
+        case pressed:
+          if (t - _timer >= 500) {
+            mode = held;
+            _timer = t;
+            _held = 1;
+           _clicksWithHeld = _clicks - 1;
+          }
+          break;
+        case held:
+          _hold = 1;
+          _clicksWithHold = _clicks - 1;
+          break;
+        case released:
+          mode = pressed;
+          _timer = t;
+          _clicks ++;
+          _clicked = 1;
+          break;
+      }
+    } else {
+      switch (mode) {
+        case pressed: mode = released; _timer = t; break;
+        case held: mode = await; _timer = t; _hold = 0; break;
+        case released: if (t - _timer >= 500) {
+            mode = await;
+            _timer = t;
+            _clicksEnd = _clicks;
+            _clicks = 0;
+          } break;
+      }
     }
   }
-}
-lastState = reading;
+  _lastState = reading;
 }
--- src/SButton.h
+++ src/SButton.h
@@ -3,24 +3,32 @@
 #include "Arduino.h"
 class Sbutton {
   public:
-    Sbutton(byte Npin, byte Npinmode) : pin(Npin), pinmode(Npinmode) {
-      pinMode(pin, (pinmode) ? INPUT : INPUT_PULLUP);
+    Sbutton(byte Npin, byte Npinmode) : _pin(Npin), _pinmode(Npinmode) {
+      pinMode(_pin, (_pinmode) ? INPUT : INPUT_PULLUP);
     };
     void tick();
-    bool isPressed;
-    bool isClicked;
-    bool isHolded;
-    bool isHold;
-    uint8_t hasClicks;
-    uint8_t isHoldAfterClicks;
+    bool isPressed(){return _pressed;}
+    bool isClicked(){return _clicked;}
+    bool isHeld(){return _held;}
+    bool isHold(){return _hold;}
+    uint8_t hasClicks(){return _clicksEnd;}
+    uint8_t hasClicksWithHeld(){return _clicksWithHeld;}
+    uint8_t hasClicksWithHold(){return _clicksWithHold;}
 
   private:
-    uint8_t pin;
-    bool pinmode;
-    uint32_t timer;
-    uint8_t clicks;
-    bool lastState = 0;
-    uint32_t lastChange;
+    bool _pinmode:1;
+    bool _lastState:1;
+    bool _pressed:1;
+    bool _clicked:1;
+    bool _held:1;
+    bool _hold:1;
+    uint8_t _pin;
+    uint8_t _clicks;
+    uint8_t _clicksEnd;
+    uint8_t _clicksWithHeld;
+    uint8_t _clicksWithHold;
     enum button_state {await, pressed, held, released} mode;
+    uint32_t _timer;
+    uint32_t _lastChange;
 };
 #endif
